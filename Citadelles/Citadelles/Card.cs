59cb8e77804325d957ba36b84d4949800fac704d
Valentin Mancier <mancier.v33@gmail.com>
2017-08-20 10:28:36 +0200

    Paquet de cartes avec cartes de base

--- Citadelles/Citadelles/Card.cs
+++ Citadelles/Citadelles/Card.cs
@@ -11,17 +11,125 @@ namespace Citadelles
     {
         private Colors _color;
         private int _cost;
+        private string _name;
 
-        public Card()
+        public Colors Color
         {
+            get
+            {
+                return _color;
+            }
 
+            set
+            {
+                _color = value;
+            }
+        }
+        public int Cost
+        {
+            get
+            {
+                return _cost;
+            }
+
+            set
+            {
+                _cost = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
         }
 
-        public void effect()
+        public Card(string name, int cost , Colors color)
+        {
+            Name = name;
+            Cost = cost;
+            Color = color;
+        }
+
+        public virtual void Effect()
         { 
 
         }
-     
+
+        public static Card Temple()
+        {
+            return new Card("Temple", 1, Colors.Blue);
+        }
+        public static Card Eglise()
+        {
+            return new Card("Eglise", 2, Colors.Blue);
+        }
+        public static Card Monastere()
+        {
+            return new Card("Monastère", 3, Colors.Blue);
+        }
+        public static Card Cathedrale()
+        {
+            return new Card("Cathédrale", 5, Colors.Blue);
+        }
+        public static Card Manoir()
+        {
+            return new Card("Manoir", 3, Colors.Yellow);
+        }
+        public static Card Chateau()
+        {
+            return new Card("Chateau", 4, Colors.Yellow);
+        }
+        public static Card Palais()
+        {
+            return new Card("Palais", 5, Colors.Yellow);
+        }
+        public static Card Taverne()
+        {
+            return new Card("Palais", 1, Colors.Yellow);
+        }
+        public static Card Echoppe()
+        {
+            return new Card("Echoppe", 2, Colors.Green);
+        }
+        public static Card Marche()
+        {
+            return new Card("Marché", 2, Colors.Green);
+        }
+        public static Card Comptoir()
+        {
+            return new Card("Comptoir", 3, Colors.Green);
+        }
+        public static Card Port()
+        {
+            return new Card("Port", 4, Colors.Green);
+        }
+        public static Card HotelDeVille()
+        {
+            return new Card("Hôtel de ville", 5, Colors.Green);
+        }
+        public static Card TourDeGuet()
+        {
+            return new Card("Tour de guet", 1, Colors.Red);
+        }
+        public static Card Prison()
+        {
+            return new Card("Prison", 2, Colors.Red);
+        }
+        public static Card Caserne()
+        {
+            return new Card("Caserne", 3, Colors.Red);
+        }
+        public static Card Forteresse()
+        {
+            return new Card("Forteresse", 5, Colors.Red);
+        }
 
     }
 }
--- Citadelles/Citadelles/Game.cs
+++ Citadelles/Citadelles/Game.cs
@@ -15,9 +15,96 @@ namespace Citadelles
 
         public Game()
         {
+            LoadLibrary();
+            Console.WriteLine(_library.Count);
+            Console.ReadLine();
             //créer les cartes et les mettres dans la librairy
             //Créer les joueurs
             //Distribuer les cartes d
+        }
+
+        //Initialise le paquet de cartes
+        public void LoadLibrary()
+        {
+            _library = new List<Card>();
+
+            //Cartes Bleues
+            for (int i = 0; i < 3; i++){
+                _library.Add(Card.Temple());
+            }
+            for (int i = 0; i < 4; i++){
+                _library.Add(Card.Eglise());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Monastere());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                _library.Add(Card.Cathedrale());
+            }
+
+            //Cartes Jaunes
+            for (int i = 0; i < 5; i++)
+            {
+                _library.Add(Card.Manoir());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                _library.Add(Card.Chateau());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                _library.Add(Card.Palais());
+            }
+
+            //Cartes Vertes
+            for (int i = 0; i < 5; i++)
+            {
+                _library.Add(Card.Taverne());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Echoppe());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                _library.Add(Card.Marche());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Comptoir());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Port());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                _library.Add(Card.HotelDeVille());
+            }
+
+            //Cartes Rouges
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.TourDeGuet());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Prison());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                _library.Add(Card.Caserne());
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                _library.Add(Card.Forteresse());
+            }
+
+            //Merveilles
+            // A lister
+
         }
     }
 }
